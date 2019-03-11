8b478cc86ffed4eb5c96e8e0bba5dc45b3ca9ff3
shalkovy <shalkovy@gmail.com>
2019-03-11 22:16:04 +0300

    first commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+        //What should happen when a non-number button is pressed
     
     @IBOutlet weak var displayLabel: UILabel!
     
     private var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-        
-        //What should happen when a non-number button is pressed
+    
         isFinishedTypingNumber = true
         
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
         //What should happen when a number is entered into the keypad
+        
         if let numValue = sender.currentTitle {
             
             if isFinishedTypingNumber == true {
