a2ae0c67a591fb765f0fdab8aec363d3d4fa6536
shalkovy <shalkovy@gmail.com>
2019-03-11 22:08:01 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Calculator
+///
+//  Created by Dima Shelkov on 3/11/19.
+//  Copyright © 2019 Dima Shelkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var isFinishedTypingNumber: Bool = true
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+        
+    }
+
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber == true {
+                
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+            
+        }
+    
+    }
+
+}
+
